6032b418e280a7b5c443fe844e4cfeb21a045fed
Jash-1th <jaswanthmamilla@gmail.com>
2025-08-10 19:34:05 +0530

    ready for deployment

--- src/db.js
+++ src/db.js
@@ -1,12 +1,21 @@
-
 const mysql = require('mysql2/promise');
-require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 27857,
+  ssl: {
+    ca: process.env.DB_SSL_CA, 
+    rejectUnauthorized: true
+  },
+  connectTimeout: 10000, 
 });
+
+
+pool.query("SELECT 1")
+  .then(() => console.log('✅ MySQL Connected'))
+  .catch(err => console.error(' MySQL Connection Failed:', err));
 
 module.exports = pool;
